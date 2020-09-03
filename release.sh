bb4c9621cce7583d2ee70b01097aee462088031e
zhangheng <zhangheng@rouchi.com>
2020-09-03 16:48:01 +0800

    0

--- release.sh
+++ release.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-
-function obtain_git_branch {
-  br=`git branch | grep "*"`
-  echo ${br/* /}
-}
-result=`obtain_git_branch`
-echo Current git branch is $result
 git add ./
-git commit -m $result
+current=`date "+%Y-%m-%d %H:%M:%S"`
+timeStamp=`date -d "$current" +%s`
+#将current转换为时间戳，精确到毫秒
+currentTimeStamp=$((timeStamp*1000+`date "+%N"`/1000000))
+git commit -m $currentTimeStamp
 git push
